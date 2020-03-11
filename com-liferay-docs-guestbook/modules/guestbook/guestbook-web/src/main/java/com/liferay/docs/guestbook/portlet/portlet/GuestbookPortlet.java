16967be3d998dd372b6bd3d7182afb48da1947cc
khangnn <nguyenngockhang96@gmail.com>
2020-03-11 12:20:06 +0700

    Defining the Component Metadata Properties

--- com-liferay-docs-guestbook/modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
+++ com-liferay-docs-guestbook/modules/guestbook/guestbook-web/src/main/java/com/liferay/docs/guestbook/portlet/portlet/GuestbookPortlet.java
@@ -1,7 +1,5 @@
 package com.liferay.docs.guestbook.portlet.portlet;
 
-import com.liferay.docs.guestbook.portlet.constants.GuestbookPortletKeys;
-
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 import javax.portlet.Portlet;
@@ -12,19 +10,21 @@ import org.osgi.service.component.annotations.Component;
  * @author apple
  */
 @Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=Guestbook",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + GuestbookPortletKeys.GUESTBOOK,
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
+    immediate = true,
+    property = {
+      "com.liferay.portlet.display-category=category.social",
+      "com.liferay.portlet.instanceable=false",
+      "com.liferay.portlet.scopeable=true",
+      "javax.portlet.display-name=Guestbook",
+      "javax.portlet.expiration-cache=0",
+      "javax.portlet.init-param.template-path=/",
+      "javax.portlet.init-param.view-template=/guestbook/view.jsp",
+      "javax.portlet.resource-bundle=content.Language",
+      "javax.portlet.security-role-ref=power-user,user",
+      "javax.portlet.supports.mime-type=text/html"
+    },
+    service = Portlet.class
 )
+
 public class GuestbookPortlet extends MVCPortlet {
 }
